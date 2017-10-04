cab14cd37146146c76958033931221eb778da5e3
dariashifrina <dshifrina@stuy.edu>
2017-10-03 20:11:10 -0400

    finished strcpy and strchr

--- work05.c
+++ work05.c
@@ -5,6 +5,7 @@
 int mystrlen( char * );
 int mystrcmp(char *, char *);
 char * mystrchr( char *s, char c );
+char * mystrcpy( char *dest, char *source );
 
 //string length fxn
 int mystrlen( char * woo){
@@ -13,6 +14,16 @@ int mystrlen( char * woo){
     len++;
       }
   return len;
+}
+
+//strcpy: functioncopies the string pointed to by source to the buffer pointed to by dest. strings may not overlap.
+
+char * mystrcpy(char *dest, char *source){
+  char *returnval = dest;
+  while(*dest++ = *source++){
+    ;
+  }
+  return returnval; 
 }
 
 /*strcmp fxn: compares two strings. Returns 0 if they are equal, returns
@@ -31,13 +42,28 @@ int mystrcmp(char *s1, char *s2){
 
 //strchr fxn: returns a pointer to the first occurrence of the cahracter c in string s
 
-int
+char * mystrchr( char *s, char c){
+  char *pointer = s;
+  while(*pointer){
+    if(*pointer == c){
+      return pointer;
+    }
+    pointer++;
+  }
+  return NULL;
+}
+
+
 
 int main(){
   char s1[200] = "hello";
   char s2[200] = "goodbye";
-  char s3[200] = "pew";
+  char s3[] = "This is a test";
+  char s4[100];
+  char s5[100];
   printf("testing mystrlen(s2):\n[standard]:%lu\n[mine]:%d\n", strlen(s2), mystrlen(s2));
   printf("testing mystrcmp(s1,s2):\n[standard]:%d\n[mine]:%d\n", strcmp(s1,s2), mystrcmp(s1,s2));
+  printf("testing mystrchr(s3,'t'):\n[standard]:%s\n[mine]:%s\n", strchr(s3,'t'), mystrchr(s3,'t'));
+  printf("testing mystrcpy(s4,s1):\n[standard]:%s\n[mine]:%s\ns4:%s\ns5:%s\n", strcpy(s4,s1),mystrcpy(s5,s1),s4, s5);
   return 0;
 }
